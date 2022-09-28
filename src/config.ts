9a941d2be9d53173a852d1a4b434df324ba20e33
Sudeep Biswas <w3bdev@protonmail.com>
2022-09-28 22:20:32 +0530

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Master of None';
+export const SITE_DESCRIPTION = 'Sharing beginner perspectives';
